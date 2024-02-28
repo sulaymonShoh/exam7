58cde650c20acbbd04efde7983949b706b241562
Sulaymonqori <ms13061312@gmail.com>
2024-02-28 20:30:50 +0500

    models created

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.FileField(upload_to="users/avatar/%Y/%m/%d")
--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.shared.models import AbstractModel
+
+
+class Category(AbstractModel):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(AbstractModel):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, models.CASCADE, "products")
+    category = models.ForeignKey(Category, models.CASCADE, "products")
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price_in_dollar = models.DecimalField(max_digits=10, decimal_places=2)
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return "{0} by {1}".format(self.name, self.owner)
